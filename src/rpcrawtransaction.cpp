36d5adf96d4b6ce4b2fe1a6d750be3e17d94b769
RPICOIN <38820452+rpicoin@users.noreply.github.com>
2018-06-15 00:01:21 +0200

    bugfix gettransaction call rpc

--- src/rpcrawtransaction.cpp
+++ src/rpcrawtransaction.cpp
@@ -50,9 +50,9 @@ void ScriptPubKeyToJSON(const CScript& scriptPubKey, Object& out, bool fIncludeH
 
 void TxToJSON(const CTransaction& tx, const uint256 hashBlock, Object& entry)
 {
-    entry.push_back(Pair("txid", tx.GetHash().GetHex()));
+//    entry.push_back(Pair("txid", tx.GetHash().GetHex()));
     entry.push_back(Pair("version", tx.nVersion));
-    entry.push_back(Pair("time", (int64_t)tx.nTime));
+//    entry.push_back(Pair("time", (int64_t)tx.nTime));
     entry.push_back(Pair("locktime", (int64_t)tx.nLockTime));
     Array vin;
     BOOST_FOREACH(const CTxIn& txin, tx.vin)
