ed6bcd229e65dc9167e300c3e8b8c9bc3871685e
THETCR <thetcr@live.nl>
2018-09-04 13:30:54 +0200

    Pubkey

--- src/pubkey.cpp
+++ src/pubkey.cpp
@@ -192,7 +192,7 @@ bool CPubKey::RecoverCompact(const uint256& hash, const std::vector<unsigned cha
 {
     if (vchSig.size() != COMPACT_SIGNATURE_SIZE)
         return false;
-    int recid = (vchSig[0] - 27) & ~4;
+    int recid = (vchSig[0] - 27) & 3;
     bool fComp = ((vchSig[0] - 27) & 4) != 0;
     secp256k1_pubkey pubkey;
     secp256k1_ecdsa_recoverable_signature sig;
