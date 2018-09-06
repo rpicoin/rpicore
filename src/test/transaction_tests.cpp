b7fc56ed7087480783be06242cf3039b103a224b
THETCR <thetcr@live.nl>
2018-09-06 10:49:17 +0200

    [TESTS] Transactions

--- src/test/transaction_tests.cpp
+++ src/test/transaction_tests.cpp
@@ -207,7 +207,7 @@ BOOST_AUTO_TEST_CASE(tx_invalid)
                 }
 
                 string transaction = test[1].get_str();
-                printf("Transactions : %s", transaction);
+                printf("Transactions : %s\n", transaction.c_str());
                 CDataStream stream(ParseHex(transaction), SER_NETWORK, PROTOCOL_VERSION);
                 CTransaction tx;
                 unsigned int nTime = 0;
