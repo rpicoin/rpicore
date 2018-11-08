4053f64c2f9aaed3a6147275e1b0d3c732a7b997
THETCR <thetcr@live.nl>
2018-11-08 14:23:27 +0100

    [DEPENDS] Further update dependencies.

--- depends/packages/packages.mk
+++ depends/packages/packages.mk
@@ -1,10 +1,11 @@
-packages:=boost openssl libevent zeromq gmp
-native_packages := native_ccache
+packages:=boost openssl libevent zeromq
 
 qt_native_packages = native_protobuf
 qt_packages = qrencode protobuf zlib
 
 qt_linux_packages:=qt expat dbus libxcb xcb_proto libXau xproto freetype fontconfig libX11 xextproto libXext xtrans
+
+rapidcheck_packages = rapidcheck
 
 qt_darwin_packages=qt
 qt_mingw32_packages=qt
