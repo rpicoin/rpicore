f820c449b0c25bffacea475c4ec2cd27efc6fa36
TCR <thetcr@live.nl>
2019-07-03 01:03:53 +0200

    [ACC] Add accumulator checksum

--- src/zpiv/accumulatorcheckpoints.json.h
+++ src/zpiv/accumulatorcheckpoints.json.h
@@ -9,6 +9,17 @@
 std::string GetMainCheckpoints() {
     std::string strMainCheckpoints = "[\n"
                                      "  {\n"
+                                     "    \"height\": 400002,\n"
+                                     "    \"1\": \"0\",\n"
+                                     "    \"5\": \"0\",\n"
+                                     "    \"10\": \"0\",\n"
+                                     "    \"50\": \"0\",\n"
+                                     "    \"100\": \"909c6636106f3c1a9e3271dd8b97f6124eb1934e65573545888c625b749ece88f13c976697225364a33070df0a1d63422bc14036584f62f3ef6f9d19b9c715b8bb41af49a61500d87044c5f5927f54592d080fe7882227128ccd037c86764f25d9a08a50bf89b6a13fdd0a869e85a223a00c39341adbbfc7ca132bbefd4c4a77\",\n"
+                                     "    \"500\": \"0\",\n"
+                                     "    \"1000\": \"0\",\n"
+                                     "    \"5000\": \"0\"\n"
+                                     "  },\n"
+                                     "  {\n"
                                      "    \"height\": 400475,\n"
                                      "    \"1\": \"c3cf46df7238e76355f1b9394d8375a7e633c39a048c69264381e3a7e4c7b51b10f51ed39912d4be029903b4ab379a5488b15567429b2b9f974c6b4f94e3806869cb90682c5311c82c6e6c5d629906c781dfce194e78c8951da202162aca6db2b095d02d297202e1e533bf740e92a328a826fe03b195cc6cc0f453b7b4f53ead\",\n"
                                      "    \"5\": \"509f41d6afc1202e025ef4f50ee7ca24391c83d40327d5f623419b65b3c5b4322ee3417ccbac00a3c892e9783ccc9cacc77eef59748a118e25d5d888be7a22dcff1a61eb68f723b4ac70e202581ecf06a4882f933b57d4186902916d06fb85b3f163a3d43743ac3b66197cb3529d16efc60e556f8dc9be4f66d7f96efad187f5\",\n"
@@ -18,7 +29,7 @@ std::string GetMainCheckpoints() {
                                      "    \"500\": \"b11c1ead0ca762e90fbe49f5229f9bdf7f56ebb1081a7af2017b502190ce5c07ffc84137047ccb24994bb32ac6ba0ad12d4ef8cf8872317b0ad8c87c10f6c1063163b6ea33ef1b719f4fd736d5e02ede89aa48d6c301855a42a946ec012eb17b84e80bae9f91bc712425d13545e2975b47e616ea34fb93c42048574a0283fb1b\",\n"
                                      "    \"1000\": \"ce2af48b6e270499e69aed438af1bba0874acfe381d4fab2e67d030f4e3ccdb232520c249bb8065a5b716de60ebeaa7134968bd131e1ec3460a253f5057cd47b753fe7b2118697e9968b07107bd151dc20a8f6a3278416514aab9bcdcb2642a5ebd26cfbe28d545ed2a383efec4d7287e53d18ae2ea56dac5b62cf6e54035dc3\",\n"
                                      "    \"5000\": \"1a38ac28dce1d2e52d81bef524cd5bb80754a40704ae3b157763d9d4b304c24d37b85639ffed8fd6eff7d0c53230c0697e9fed4eba8ac7aca20ed199a26b5b4ce1d6d70f1528657b95ce18f217dec87e626d6b626ef03fc0d9128ca51dcf120100b642f98c606fa8749e00ae2fec7fdeaf82b567a513ccc1db8e03f0d5eed21d\"\n"
-                                     "  }\n"
+                                     "  },\n"
                                      "]";
     return strMainCheckpoints;
 }
